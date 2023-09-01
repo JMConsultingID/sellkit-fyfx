c9f4eddcd27f19d142a30c87a4efda941bca44a6
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-02 00:10:46 +0800

    Update sellkit-fyfx.php

--- sellkit-fyfx.php
+++ sellkit-fyfx.php
@@ -63,7 +63,7 @@ register_deactivation_hook( __FILE__, 'deactivate_sellkit_fyfx' );
  * admin-specific hooks, and public-facing site hooks.
  */
 require plugin_dir_path( __FILE__ ) . 'includes/class-sellkit-fyfx.php';
-
+require plugin_dir_path( __FILE__ ) . 'includes/class-sellkit-fyfx-functions.php';
 /**
  * Begins execution of the plugin.
  *
