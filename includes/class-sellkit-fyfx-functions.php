cb7f624f01070d7b5d1841c3364d840f8d1ae5b9
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-02 01:21:17 +0800

    Update class-sellkit-fyfx-functions.php

--- includes/class-sellkit-fyfx-functions.php
+++ includes/class-sellkit-fyfx-functions.php
@@ -53,7 +53,7 @@ function replace_sellkit_action() {
         
     }
 }
-add_action( 'plugins_loaded', 'replace_sellkit_action', 9999 );
+add_action( 'init', 'replace_sellkit_action',9999 );
 
 // Fungsi baru Anda yang akan menggantikan first_step_begin
 function my_new_function() {
