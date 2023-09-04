d88bad7daade9cdd6ec486a26291053fe5675c96
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-04 11:20:12 +0800

    Update class-sellkit-fyfx-functions-frontend.php

--- includes/class-sellkit-fyfx-functions-frontend.php
+++ includes/class-sellkit-fyfx-functions-frontend.php
@@ -36,12 +36,14 @@ function sellkit_fyfx_add_terms_and_conditions_checkbox() {
     $checkout = WC_Checkout::instance();
 
     if ($checkout->get_value('terms') && apply_filters('woocommerce_checkout_show_terms', true) && function_exists('wc_terms_and_conditions_checkbox_enabled') && wc_terms_and_conditions_checkbox_enabled()) {
-        wc_get_template('checkout/terms.php');
+        if (get_option('sellkit_fyfx_enable_terms_conditions') === 'enable') {
+            wc_get_template('checkout/terms.php');
+        } else {
+            echo "";
+        }
     }
 }
-if (get_option('sellkit_fyfx_enable_terms_conditions') === 'enable') {
 add_action('wp_footer', 'sellkit_fyfx_add_terms_and_conditions_checkbox');
-}
 
 function sellkit_fyfx_enqueue_frontend_scripts() {
     // Cek apakah plugin diaktifkan
