e675c352e00455080b6d4f0cf2eacca208db64e3
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-04 15:21:18 +0800

    Update class-sellkit-fyfx.php

--- includes/class-sellkit-fyfx.php
+++ includes/class-sellkit-fyfx.php
@@ -103,24 +103,24 @@ class Sellkit_Fyfx {
 		 * The class responsible for orchestrating the actions and filters of the
 		 * core plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-sellkit-ypf-loader.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-sellkit-fyfx-loader.php';
 
 		/**
 		 * The class responsible for defining internationalization functionality
 		 * of the plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-sellkit-ypf-i18n.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-sellkit-fyfx-i18n.php';
 
 		/**
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-sellkit-ypf-admin.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-sellkit-fyfx-admin.php';
 
 		/**
 		 * The class responsible for defining all actions that occur in the public-facing
 		 * side of the site.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-sellkit-ypf-public.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-sellkit-fyfx-public.php';
 
 		$this->loader = new Sellkit_Fyfx_Loader();
 
