161796ace0f7007f1bdf56945f70ebc66ad5a857
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-04 09:21:29 +0800

    Update class-sellkit-fyfx.php

--- includes/class-sellkit-fyfx.php
+++ includes/class-sellkit-fyfx.php
@@ -169,9 +169,10 @@ class Sellkit_Fyfx {
 	private function define_public_hooks() {
 
 		$plugin_public = new Sellkit_Fyfx_Public( $this->get_plugin_name(), $this->get_version() );
-
+		if (get_option('sellkit_fyfx_enable_plugin') == 'enable') {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles', 100 );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts', 100 );
+		}
 
 	}
 
