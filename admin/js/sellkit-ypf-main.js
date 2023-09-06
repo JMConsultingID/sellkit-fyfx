d8a02ae349cfa79ebcf3e655db7621525be9948b
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-06 12:26:47 +0800

    Update sellkit-ypf-main.js

--- admin/js/sellkit-ypf-main.js
+++ admin/js/sellkit-ypf-main.js
@@ -1,36 +1,45 @@
 (function( $ ) {
 	'use strict';
 
-jQuery(document).ready(function($) {
-    $('#sellkit-ypf-add-badge-button').click(function(e) {
-        e.preventDefault();
-
-        var frame = wp.media({
-            title: 'Select or Upload Badge',
-            button: {
-                text: 'Use this badge'
-            },
-            multiple: false
-        });
-
-        frame.on('select', function() {
-            var attachment = frame.state().get('selection').first().toJSON();
-            var badgeHTML = `
-                <div class="sellkit-ypf-badge">
-                    <input type="hidden" name="sellkit_ypf_badges_images_payment[]" value="${attachment.url}" />
-                    <img src="${attachment.url}" style="max-width:100px; display:block;" />
-                    <button type="button" class="button sellkit-ypf-remove-badge-button">Remove</button>
-                </div>
-            `;
-            $('.sellkit-ypf-badges-wrapper').append(badgeHTML);
-        });
-
-        frame.open();
+	document.addEventListener('DOMContentLoaded', function() {
+        var exportButton = document.getElementById('sellkit-ypf-export');
+        if (exportButton) {
+            exportButton.addEventListener('click', function() {
+                window.location.href = '<?php echo admin_url('admin.php?page=sellkit-ypf&sellkit_ypf_export=true'); ?>';
+            });
+        }
     });
 
-    $(document).on('click', '.sellkit-ypf-remove-badge-button', function() {
-        $(this).closest('.sellkit-ypf-badge').remove();
-    });
-});
+	jQuery(document).ready(function($) {
+	    $('#sellkit-ypf-add-badge-button').click(function(e) {
+	        e.preventDefault();
+
+	        var frame = wp.media({
+	            title: 'Select or Upload Badge',
+	            button: {
+	                text: 'Use this badge'
+	            },
+	            multiple: false
+	        });
+
+	        frame.on('select', function() {
+	            var attachment = frame.state().get('selection').first().toJSON();
+	            var badgeHTML = `
+	                <div class="sellkit-ypf-badge">
+	                    <input type="hidden" name="sellkit_ypf_badges_images_payment[]" value="${attachment.url}" />
+	                    <img src="${attachment.url}" style="max-width:100px; display:block;" />
+	                    <button type="button" class="button sellkit-ypf-remove-badge-button">Remove</button>
+	                </div>
+	            `;
+	            $('.sellkit-ypf-badges-wrapper').append(badgeHTML);
+	        });
+
+	        frame.open();
+	    });
+
+	    $(document).on('click', '.sellkit-ypf-remove-badge-button', function() {
+	        $(this).closest('.sellkit-ypf-badge').remove();
+	    });
+	});
 
 })( jQuery );
